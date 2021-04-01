349b40074fdf4e51b8b3c254f8313087ce86e0a4
Tanner Bennett <tannerbennett@me.com>
2021-04-01 17:11:42 -0500

    Adopt DocumentManager and HopperBootstrap

--- src/extension.ts
+++ src/extension.ts
@@ -11,34 +11,16 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import HopperClient from './api/hopper';
-import { HooksProvider } from './views/hooks';
-import { ProceduresProvider } from './views/procs';
-import { StringsProvider } from './views/strings';
+import HopperBootstrap from './bootstrap-hopper';
+import DocumentManager from './document-manager';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    
-    vscode.window.registerTreeDataProvider('procs', new ProceduresProvider());
-    // vscode.window.registerTreeDataProvider('hooks', new HooksProvider());
-    vscode.window.registerTreeDataProvider('strings', new StringsProvider());
-    
-    // Register a content provider for the hopper-scheme
-	const myScheme = 'hopper';
-	const myProvider = new class implements vscode.TextDocumentContentProvider {
-
-		// Emitter and its event
-		onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
-		onDidChange = this.onDidChangeEmitter.event;
-
-		provideTextDocumentContent(uri: vscode.Uri): vscode.ProviderResult<string> {
-            const parts = uri.path.split('/');
-            const segment = parts[0];
-            const address = parseInt(parts[1]);
-			return HopperClient.shared.decompileProcedure(segment, address);
-		}
-	};
-	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(myScheme, myProvider));
+    HopperBootstrap.extensionPath = context.extensionPath;
+	
+	DocumentManager.shared.registerViews();
+	DocumentManager.shared.registerDocumentProviders(context);
 
 	// Register a command that opens a Hopper document
 	context.subscriptions.push(vscode.commands.registerCommand('hopper.open', async (path: string) => {
