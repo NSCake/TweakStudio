96aa5402a07a10c56c846149d98285e47487368c
Tanner Bennett <tannerbennett@me.com>
2021-06-06 01:41:02 -0500

    Simplify command registration

--- src/extension.ts
+++ src/extension.ts
@@ -7,8 +7,6 @@
 //
 
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { window, workspace, commands, Uri } from 'vscode';
 import HopperClient from './api/hopper';
@@ -35,9 +33,17 @@ const IDAFamily: DisassemblerFamily = {
     bootstrap: IdaBootstrap
 };
 
+interface ContextExtended extends vscode.ExtensionContext {
+    registerCommand(cmd: string, callback: (...args: any[]) => any);
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ContextExtended) {
+    context.registerCommand = function(this: ContextExtended, cmd: string, cb) {
+        context.registerCommand(cmd, cb);
+    };
+    
     // Read settings
     reloadSettings();
     let path = workspace.getConfiguration('tweakstudio.hopper').get('path');
@@ -53,31 +59,31 @@ export function activate(context: vscode.ExtensionContext) {
     // Register commands //
     
     // Open a new document/database/binary in Hopper or IDA
-    context.subscriptions.push(commands.registerCommand('hopper.open', async () => {
+    context.registerCommand('hopper.open', async () => {
         DocumentManager.shared.promptToStartNewClient(HopperFamily);
-    }));
-    context.subscriptions.push(commands.registerCommand('ida.open', async () => {
+    });
+    context.registerCommand('ida.open', async () => {
         DocumentManager.shared.promptToStartNewClient(IDAFamily);
-    }));
+    });
     
     // For debugging, quickly open a dummy binary
-    context.subscriptions.push(commands.registerCommand('hopper.open-test', async () => {
+    context.registerCommand('hopper.open-test', async () => {
         DocumentManager.shared.startNewClient(HopperBootstrap.testBinary, HopperFamily);
-    }));
-    context.subscriptions.push(commands.registerCommand('ida.open-test', async () => {
+    });
+    context.registerCommand('ida.open-test', async () => {
         DocumentManager.shared.startNewClient(HopperBootstrap.testBinary, IDAFamily);
-    }));
+    });
 
     // Open a pseudocode document
-    context.subscriptions.push(commands.registerCommand('hopper.view-pseudocode', async (path: string, lineno?: number) => {
+    context.registerCommand('hopper.view-pseudocode', async (path: string, lineno?: number) => {
         DocumentManager.shared.showDocument(Uri.parse('hopper:' + path), lineno);
-    }));
-    context.subscriptions.push(commands.registerCommand('ida.view-pseudocode', async (path: string, lineno?: number) => {
+    });
+    context.registerCommand('ida.view-pseudocode', async (path: string, lineno?: number) => {
         // DocumentManager.shared.switchToClient()
         const port = DocumentManager.shared.activeClient.id;
         const uri = Uri.parse('ida:' + path).with({ query: port });
         DocumentManager.shared.showDocument(uri, lineno);
-    }));
+    });
     
     function showXrefPicker(refs: Xref[]) {
         const quickPick = window.createQuickPick();
@@ -91,15 +97,15 @@ export function activate(context: vscode.ExtensionContext) {
     }
     
     // Show selrefs from selector strings
-    context.subscriptions.push(commands.registerCommand('ida.show-selrefs', async (address: number) => {
+    context.registerCommand('ida.show-selrefs', async (address: number) => {
         const refs = await DocumentManager.shared.activeClient.listSelrefs(address);
         showXrefPicker(refs);
-    }));
+    });
     // Show xrefs from everything else
-    context.subscriptions.push(commands.registerCommand('ida.show-xrefs', async (address: number) => {
+    context.registerCommand('ida.show-xrefs', async (address: number) => {
         const refs = await DocumentManager.shared.activeClient.listXrefs(address);
         showXrefPicker(refs);
-    }));
+    });
 }
 
 export function deactivate(context: vscode.ExtensionContext) {
